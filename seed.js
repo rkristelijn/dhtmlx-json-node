89167c4fb9d96efc185fe952691cc1d71c923fb8
Remi Kristelijn <rkristelijn@gmail.com>
2018-06-13 11:19:16 +0200

    fixed settings api and default select for events

--- seed.js
+++ seed.js
@@ -14,5 +14,6 @@ db.dropDatabase();
 require('./api/contacts/contacts-seed.js');
 require('./api/projects/projects-seed.js');
 require('./api/events/events-seed.js');
+require('./api/settings/settings-seed.js');
 
 console.log(`press CTRL+C to exit`);
